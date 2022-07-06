c6ee57ba2be02d2e8de8c448e5368b0c8116cc12
edmilton75 <edmilton7500@gmail.com>
2022-07-06 15:46:58 -0300

    Configuração do botão random

--- src/Components/Blockquote.jsx
+++ src/Components/Blockquote.jsx
@@ -3,21 +3,19 @@ import "../styles/ButtonRandon.css";
 
 const Blockquote = () => {
   const [random, setRandom] = useState(null);
-
+  const [atualizar, setAtualizar] = useState(0)
 
   useEffect(() => {
     fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
       .then((r) => r.json())
       .then((json) => setRandom(json));
-  }, []);
-  console.log(random);
+  }, [atualizar]);
   if (random === null) return null;
-
 
   return (
     <>
       <header>
-        <button  className="buttonRandon">Random</button>
+        <button onClick={() => setAtualizar(atualizar + 1)} className="buttonRandon">Random</button>
       </header>
       <blockquote className="blockquote">
         <p>"{random.data[0].quoteText}"</p>
